bbc0e479818dc5b0a8a7a2937164143567334215
Beomjun Kim <nutyworks@gmail.com>
2023-07-06 23:19:24 +0900

    Improve 10166 solution

--- 0x03/ex02_10166.cpp
+++ 0x03/ex02_10166.cpp
@@ -2,27 +2,24 @@
 
 using namespace std;
 
-int blocked[2001][2001];
-
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-
     int d1, d2, cnt = 0;
-    cin >> d1 >> d2;
+    scanf("%d%d", &d1, &d2);
 
     for (int r = d1; r <= d2; r++) {
         for (int th = 1; th <= r; th++) {
             int g = gcd(r, th);
+            int blocked = 0;
 
-            if (!blocked[th / g][r / g]) {
-                blocked[th / g][r / g] = 1;
-                cnt++;
+            for (int i = 1; i < g; i++) {
+                if (r * i >= d1 * g) blocked = 1;
             }
+
+            if (!blocked) cnt++;
         }
     }
 
-    cout << cnt;
+    printf("%d", cnt);
 
     return 0;
 }
